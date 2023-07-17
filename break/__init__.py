3fc8eda5e7afcd626e5761f40a5f47f6edec69e7
CJensen95 <90799234+CJensen95@users.noreply.github.com>
2023-07-17 01:28:48 -0500

    Update __init__.py

--- break/__init__.py
+++ break/__init__.py
@@ -1,5 +1,13 @@
+import json
+from pathlib import Path
+
+from redbot.core.bot import Red
+
 from .break import break
 
+with open(Path(__file).parent / "info.json") as fp:
+        __red_end_user_data_statement__ = json.load(fp)["edn_user_data_statement"]
 
-async def setup(bot):
+
+async def setup(bot: Red):
     await bot.add_cog(break(bot))
